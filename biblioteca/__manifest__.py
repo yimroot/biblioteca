46c233a084fdcc39615dbad5ebd3556919e386b1
Edison Saavedra <edisonsaavedra4@gmail.com>
2025-10-06 08:31:32 -0500

    actualizacion 2

--- biblioteca/__manifest__.py
+++ biblioteca/__manifest__.py
@@ -22,9 +22,9 @@ Long description of module's purpose
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
+        # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
@@ -32,4 +32,4 @@ Long description of module's purpose
     ],
     'application': True,
     'license' : 'AGPL-3'
-
+}
