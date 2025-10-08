3f246663b5a7c3cf2934c63e345a105274962a09
Edison Saavedra <edisonsaavedra4@gmail.com>
2025-10-08 11:13:43 -0500

    actualizacion 2025/10/08 numero 2

--- biblioteca/models/models.py
+++ biblioteca/models/models.py
@@ -5,12 +5,12 @@ from odoo import models, fields, api
 class biblioteca(models.Model):
     _name = 'biblioteca.libro'
     _description = 'biblioteca.biblioteca'
-    _rec_name='lastname'
+    _rec_name='firstname'
     
     firstname = fields.Char(string='Nombre Libro')
-    autor=fields.Many2one('biblioteca.autor' , string='Autor Libro')
+    autor = fields.Many2one('biblioteca.autor' , string='Autor Libro')
     value = fields.Integer(string='Numero ejemplares')
-    value2 = fields.Float(compute="_value_pc", store=True , string='Costo')
+    value2 = fields.Float(compute="_value_pc", store=True , string="Costo")
     description = fields.Text(string='Resumen Libro')
 
     @api.depends('value')
@@ -20,14 +20,14 @@ class biblioteca(models.Model):
 
 
 class BiliotecaAutor(models.Model):
-  _name='biblioteca.autor'
-  _description='biblioteca.autor'
+   _name='biblioteca.autor'
+   _description='biblioteca.autor'
 
-  firstname=fields.Char()
-  lastname=fields.Char()
+   firstname=fields.Char()
+   lastname=fields.Char()
   
-  @api.depends('firstname' , 'lastname' )
-  def _compute_display_name(self):
+   @api.depends('firstname' , 'lastname' )
+   def _compute_display_name(self):
         for record in self:
             record.display_name = f"{record.firstname} {record.lastname}"
             
